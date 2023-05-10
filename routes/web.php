a3f161c5bc985909d5be6ede40312886e7996510
nako.shimoe <shimoe.n@78it.com>
2023-05-10 02:02:30 +0000

    go

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,7 @@ Route::get('/', [TasksController::class, 'index']);
 
 Route::get('/dashboard', [TasksController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
 
